99827b404babb5d0e3a1a89d9e0f3408138018d8
Blag <atejada@gmail.com>
2024-03-04 10:05:52 -0500

    Update nicegui.py

--- nicegui.py
+++ nicegui.py
@@ -1,12 +1,18 @@
-from nicegui import ui
+from fastapi import FastAPI
+from nicegui import app, ui
+#import uvicorn
 
-ui.icon('thumb_up')
-ui.markdown('This is **Markdown**.')
-ui.html('This is <strong>HTML</strong>.')
-with ui.row():
-    ui.label('CSS').style('color: #888; font-weight: bold')
-    ui.label('Tailwind').classes('font-serif')
-    ui.label('Quasar').classes('q-ml-xl')
-ui.link('NiceGUI on GitHub', 'https://github.com/zauberzeug/nicegui')
+fastapi_app = FastAPI()
 
-ui.run()
+@ui.page('/')
+def index():
+    ui.icon('thumb_up')
+    ui.markdown('This is **Markdownsss**.')
+    ui.html('This is <strong>HTML</strong>.')
+    with ui.row():
+        ui.label('CSS').style('color: #888; font-weight: bold')
+        ui.label('Tailwind').classes('font-serif')
+        ui.label('Quasar').classes('q-ml-xl')
+    ui.link('NiceGUI on GitHub', 'https://github.com/zauberzeug/nicegui')
+
+ui.run_with(fastapi_app)
